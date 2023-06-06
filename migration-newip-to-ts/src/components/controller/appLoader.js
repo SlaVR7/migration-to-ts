e0d11e5e815696a07f7eca4ae841a5e8cd0976fe
SlaVR <mylife0793@gmail.com>
2023-06-06 17:18:38 +0300

    chore: install dependencies

--- migration-newip-to-ts/src/components/controller/appLoader.js
+++ migration-newip-to-ts/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '378143bd20b844c1afbbc1260be37591', // получите свой ключ https://newsapi.org/
         });
     }
 }
