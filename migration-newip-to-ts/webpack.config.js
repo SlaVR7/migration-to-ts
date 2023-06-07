3a0af922fa349c41b49faf19e9d18346554f6735
SlaVR <mylife0793@gmail.com>
2023-06-07 15:53:36 +0300

    chore: add interface

--- migration-newip-to-ts/webpack.config.js
+++ migration-newip-to-ts/webpack.config.js
@@ -29,7 +29,7 @@ const baseConfig = {
             filename: 'index.html',
         }),
         new CleanWebpackPlugin(),
-        new EslingPlugin({ extensions: 'ts' })
+        new EslingPlugin({ extensions: 'ts' }),
     ],
 };
 
